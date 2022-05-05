2a6aeeaeacda7556be789ab4602e51e8adbdec27
VictorGonCh <launchx00643@innovaccion.mx>
2022-05-05 16:23:03 -0500

    agregado 3 lineas de prueba

--- weekly_mission_1/example1/main.js
+++ weekly_mission_1/example1/main.js
@@ -4,6 +4,7 @@ let myCar = new Object(); // Creación de un objeto
 myCar.make = 'Ford'; // Guardando un valor dentro del objeto creado
 myCar.model = 'Mustang';
 myCar.year = 1969;
+myCar.kilometraje = 50000;
 
 console.log(myCar) // Imprimiendo objeto
 
@@ -15,12 +16,14 @@ const myModule = (() => {
  const privateFoo = "Soy un valor privado, solo me usan dentro de este objeto"
  const privateBar = [1,2,3,4]
  const baz = "Soy un valor que va a ser expuesto"
+ const publicDato = "prueba datos"
 
 // Variable para guardar las variables locales
  const exported = {
    publicFoo: "Valor público, pueden verme desde donde me llamen",
    publicBar: "Otro valor público",
-   	publicBaz: baz
+   publicBaz: baz,
+   privateDato: "prueba datos exported"
  }
 
 // Exposición de variables locales
