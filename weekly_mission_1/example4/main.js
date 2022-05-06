4a795512742ff275c71e68c97ec616a557438dee
VictorGonCh <launchx00643@innovaccion.mx>
2022-05-06 01:22:44 -0500

    agregando archivos ejemplo 4

---
+++ weekly_mission_1/example4/main.js
@@ -0,0 +1,12 @@
+const Logger = require('./logger') // Invocas el módulo que contiene la clase
+
+// Creación de un objeto
+const dbLogger = new Logger('DB') // Creas un objeto nuevo, esto llama por default el constructor de la clase
+// invocación del método
+dbLogger.info('This is an informational message')
+dbLogger.verbose('otro mensaje dblogger')
+
+// Creación de otro objeto
+const accessLogger = new Logger('ACCESS')
+accessLogger.verbose('This is a verbose message')
+accessLogger.info('otro mensaje acceslogger')
